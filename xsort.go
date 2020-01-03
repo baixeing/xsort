06fef12c43da097b6e5f8f924d73ebc57137a35d
baixeing <baixeing@trojan.work>
2020-01-03 13:14:27 +0200

    _

---
+++ xsort.go
@@ -0,0 +1,49 @@
+package xsort
+
+import "reflect"
+
+func Merge(xs interface{}, f func(interface{}, interface{}) bool) {
+	merge := func(left, right reflect.Value) reflect.Value {
+		length := left.Len() + right.Len()
+		capacity := left.Cap() + right.Cap()
+		xs := reflect.MakeSlice(left.Type(), length, capacity)
+
+		for i := 0; i < length; i++ {
+			v := xs.Index()
+			if left.Len() > 0 && (right.Len() == 0 || f(left.Index(0).Interface(), right.Index(0).Interface())) {
+				v.Set(left.Index(0))
+				left = left.Slice(1, left.Len())
+			} else if right.Len() > 0 {
+				v.Set(right.Index(0))
+				right = right.Slice(1, right.Len())
+			}
+		}
+
+		return xs
+	}
+
+	sort := func(xs reflect.Value) reflect.Value {
+		if xs.Len() < 2 {
+			return xs
+		}
+
+		return merge(
+			sort(xs.Slice(0, xs.Len()/2)),
+			sort(xs.Slice(xs.Len()/2, xs.Len())),
+		)
+	}
+
+	xs = sort(reflect.ValueOf(xs), f).Interface()
+}
+
+func Insertion(xs interface{}, f func(interface{}, interface{}) bool) {
+	v := reflect.ValueOf(xs)
+
+	for i := 0; i < v.Len(); i++ {
+		for j := i; j > 0 && f(v.Index(j).Interface(), v.Index(j-1).Interface()); j-- {
+			x, y := v.Index(j).Interface(), v.Index(j-1).Interface()
+			v.Index(j).Set(reflect.ValueOf(y))
+			v.Index(j - 1).Set(reflect.ValueOf(x))
+		}
+	}
+}
